b37a8e5490c84f9d682686fcc549b4885d557df0
Yuhaigiub <buigiahuynt@gmail.com>
2022-12-21 12:45:32 +0700

    modify routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,34 @@ import { Routes, Route } from "react-router-dom";
 import LayoutWithMenu from "./components/Layouts/LayoutWithMenu";
 import LayoutWithoutMenu from "./components/Layouts/LayoutWithoutMenu";
 import TableWrapper from "./components/TableWrapper";
-import Records from "./pages/Records";
+import Home from "./pages/Home";
+import Records from "./pages/Record";
+import PlaylistRoute from "./routes/Playlist";
+import ProfileRoute from "./routes/Profile";
+import RecordRoute from "./routes/Record";
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<LayoutWithMenu />}>
-				<Route path="records">
-					<Route index element={<Records />} />
+			<Route path="/">
+				<Route index element={<Home />} />
+				<Route path="profile/*" element={<ProfileRoute />} />
+				<Route path="record/*" element={<RecordRoute />} />
+				<Route path="playlist/*" element={<PlaylistRoute />} />
+				<Route path="schedule/*"></Route>
+				<Route path="manage/*">
+					<Route path="contract" />
+					<Route path="device" />
+					<Route path="user" />
+					<Route path="company" />
 				</Route>
+				<Route path="statistic/*">
+					<Route path="report" />
+					<Route path="history" />
+					<Route path="distribute" />
+				</Route>
+				<Route path="setting/*"></Route>
+				<Route path="support"></Route>
 			</Route>
 		</Routes>
 	);
