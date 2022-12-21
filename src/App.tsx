3f92030547f48597b112a15daf11b95d748442d3
Yuhaigiub <buigiahuynt@gmail.com>
2022-12-21 10:59:45 +0700

    app change

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 import { Routes, Route } from "react-router-dom";
-import LayoutWithMenu from "./components/LayoutWithMenu";
-import LayoutWithoutMenu from "./components/LayoutWithoutMenu";
+import LayoutWithMenu from "./components/Layouts/LayoutWithMenu";
+import LayoutWithoutMenu from "./components/Layouts/LayoutWithoutMenu";
 import TableWrapper from "./components/TableWrapper";
+import Records from "./pages/Records";
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<LayoutWithMenu />}>
-				<Route path="manage">
-					<Route path="contract" element={<TableWrapper />} />
+				<Route path="records">
+					<Route index element={<Records />} />
 				</Route>
 			</Route>
 		</Routes>
