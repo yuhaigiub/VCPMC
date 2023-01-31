0455c2b804bbef1749d18379a41fc3ca544d05d9
Yuhaigiub <buigiahuynt@gmail.com>
2023-01-31 09:35:41 +0700

    extract constant

---
+++ src/constants/theme-setup.tsx
@@ -0,0 +1,33 @@
+export const AntdTheme = {
+	token: {
+		colorPrimary: "#ff7506",
+		colorBgContainer: "rgba(47, 47, 65, 0.7)",
+		colorBgElevated: "rgba(47, 47, 65)",
+		colorBorder: "rgb(67, 67, 79)",
+		colorBorderSecondary: "rgb(67, 67, 79)",
+		colorText: "#fff",
+		colorTextPlaceholder: "rgb(166, 166, 166, 0.8);",
+		colorTextDisabled: "#888",
+	},
+	components: {
+		Select: {
+			colorTextDisabled: "#ff7506",
+			colorText: "#bbb",
+		},
+		Input: {},
+		DatePicker: {
+			colorIcon: "#ff7506",
+			colorIconHover: "#fff",
+			colorTextDisabled: "#ff7506",
+		},
+		Checkbox: {
+			// colorBgContainer: "#fff",
+			colorBorder: "#347aff",
+			fontSize: 16,
+		},
+		Radio: {
+			colorBorder: "#ff7506",
+			borderRadius: 10,
+		},
+	},
+};
